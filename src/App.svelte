931965a372796820dd1add32c0d521fdd9e47574
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-10-06 20:20:31 -0300

    mas palabar & style

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,21 @@
 <script lang='ts'>
+    import { each } from "svelte/internal";
+
   const key_words={
-    OPF:['organi',  'activo'],
-    OPD:['organi', 'activo'],
+    OPF:['organi',  'activo', 'analizar', 'mejoras', 'cambios', 'oprtunidades de mejora', 'procedimientos', 'fuertes', 'endebles', 'objetivo'],
+    OPD:['organi', 'activo', 'definir', 'repositorio', 'analizado', 'cambios', 'no publicado', 'realizado', 'procedimientos' ],
     OT:['organi', 'activo'],
-    PP:['proyecto'],
-    PMC:['proyecto', 'retraso', 'reunión de avance', 'plan'],
+    PP:['proyecto','primera vez', 'inicio', 'objetivo', 'puntos de funcion', 'roles', 'plazo' ],
+    PMC:['proyecto', 'retraso', 'reunión de avance', 'plan','inconvenientes', 'problema', 'corroborar', 'Firefox', 'controles', 'mail' ],
     VAL:['proyecto', 'cliente', 'aceptacion','producto de trabajo', 'diagramas de secuencia', 'casos de uso', 'caso de uso' ],
-    VER:['proyecto','producto de trabajo','diagramas de secuencia', 'casos de uso', 'caso de uso', 'Inspección' ],
+    VER:['proyecto','producto de trabajo','diagramas de secuencia', 'casos de uso', 'caso de uso', 'inspección', 'dss' ],
   }
 
   let question=''
 
   const counts={
     OPF:0,
-    OTD:0,
+    OPD:0,
     OT:0,
     PP:0,
     PMC:0,
@@ -37,10 +39,20 @@
 </script>
 
 
-<h1>Holaa</h1>
-<textarea bind:value={question}></textarea>
+<h1>Adivinador de CMMI</h1>
 
-
+<span>Vos pega la pregunta, el que tenga el numerito am alto sera el mas probalbe (espero)</span>
 <pre>
   {JSON.stringify(counts)}
 </pre>
+<textarea bind:value={question}></textarea>
+
+
+<style>
+textarea {
+  width: 100%;
+  height: 200px;
+  font-size: 1.5em;
+}
+</style>
+
